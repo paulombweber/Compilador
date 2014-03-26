e3cdcdd4f354cf9e66c3207f410f711fe57b58ba
Paulo Weber <pauloweber@hotmail.com.br>
2014-03-25 23:38:34 -0300

    Arrumei os atalhos e começei a fazer a parte da área de mensagens que não tinha nada implementado

--- Compilador/src/gui/FrmCompilador.java
+++ Compilador/src/gui/FrmCompilador.java
@@ -6,18 +6,19 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -33,28 +34,30 @@ import javax.swing.border.LineBorder;
 import javax.swing.border.MatteBorder;
 
 public class FrmCompilador extends JFrame {
-
-	private String nomeArquivo;
-	private JTextArea taEditor;
-	private JScrollPane spMensagens;
-	private JTextArea taMensagens;
-	private JPanel paStatusBar;
-	private JLabel lblStatusbar;
-	private JPanel paEsqStatusBar;
-	private JButton btNovo;
-	private JButton btAbrir;
-	private JButton btSalvar;
-	private JButton btCopiar;
-	private JButton btColar;
-	private JButton btRecortar;
-	private JButton btCompilar;
-	private JButton btGerarCodigo;
-	private JButton btEquipe;
-	private JPanel paEditor;
-	private JScrollPane spEditor;
-
+	
+	private String				nomeArquivo	= "";
+	private JTextArea			taEditor;
+	private JScrollPane			spMensagens;
+	private JTextArea			taMensagens;
+	private JPanel				paStatusBar;
+	private JLabel				lblStatusbar;
+	private JPanel				paEsqStatusBar;
+	private JButton				btNovo;
+	private JButton				btAbrir;
+	private JButton				btSalvar;
+	private JButton				btCopiar;
+	private JButton				btColar;
+	private JButton				btRecortar;
+	private JButton				btCompilar;
+	private JButton				btGerarCodigo;
+	private JButton				btEquipe;
+	private JPanel				paEditor;
+	private JScrollPane			spEditor;
+	private ArrayList<String>	mensagens;
+	
 	public FrmCompilador() {
 		initialize();
+		mensagens = new ArrayList<>();
 	}
 	
 	/**
@@ -67,251 +70,222 @@ public class FrmCompilador extends JFrame {
 		setBounds(100, 100, 945, 450);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(new BorderLayout(0, 0));
-
+		
 		JPanel paBotoes = new JPanel();
 		getContentPane().add(paBotoes, BorderLayout.NORTH);
-
+		
 		Action acNovo = new AbstractAction() {
-			public void actionPerformed(ActionEvent arg0) {
+			
+			@Override
+			public void actionPerformed(final ActionEvent arg0) {
 				novo();
 			}
 		};
-
+		
+		Action acAbrir = new AbstractAction() {
+			
+			@Override
+			public void actionPerformed(final ActionEvent arg0) {
+				try {
+					abrir();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		
+		Action acSalvar = new AbstractAction() {
+			
+			@Override
+			public void actionPerformed(final ActionEvent arg0) {
+				try {
+					salvar();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		
+		Action acCopiar = new AbstractAction() {
+			
+			@Override
+			public void actionPerformed(final ActionEvent arg0) {
+				copiar();
+			}
+		};
+		
+		Action acColar = new AbstractAction() {
+			
+			@Override
+			public void actionPerformed(final ActionEvent arg0) {
+				colar();
+			}
+		};
+		
+		Action acRecortar = new AbstractAction() {
+			
+			@Override
+			public void actionPerformed(final ActionEvent arg0) {
+				recortar();
+			}
+		};
+		
+		Action acCompilar = new AbstractAction() {
+			
+			@Override
+			public void actionPerformed(final ActionEvent arg0) {
+				compilar();
+			}
+		};
+		
+		Action acGerarCodigo = new AbstractAction() {
+			
+			@Override
+			public void actionPerformed(final ActionEvent arg0) {
+				gerarCodigo();
+			}
+		};
+		
+		Action acEquipe = new AbstractAction() {
+			
+			@Override
+			public void actionPerformed(final ActionEvent arg0) {
+				equipe();
+			}
+		};
+		
+		ActionMap acoes = paBotoes.getActionMap();
+		acoes.put("btNovo", acNovo);
+		acoes.put("btAbrir", acAbrir);
+		acoes.put("btSalvar", acSalvar);
+		acoes.put("btCopiar", acCopiar);
+		acoes.put("btColar", acColar);
+		acoes.put("btRecortar", acRecortar);
+		acoes.put("btCompilar", acCompilar);
+		acoes.put("btGerarCodigo", acGerarCodigo);
+		acoes.put("btEquipe", acEquipe);
+		
+		paBotoes.setActionMap(acoes);
+		InputMap atalhos = paBotoes.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW);
+		atalhos.put(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK), "btNovo");
+		// o Ctrl A deve sobrescrever o tratamento do JTextArea (Selecionar
+		// tudo)
+		atalhos.put(KeyStroke.getKeyStroke(KeyEvent.VK_A, KeyEvent.CTRL_DOWN_MASK), "btAbrir");
+		atalhos.put(KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK), "btSalvar");
+		
+		// begin - Verificar se vamos 'retratar' ou deixar o JTextArea tratar
+		atalhos.put(KeyStroke.getKeyStroke(KeyEvent.VK_C, KeyEvent.CTRL_DOWN_MASK), "btCopiar");
+		atalhos.put(KeyStroke.getKeyStroke(KeyEvent.VK_V, KeyEvent.CTRL_DOWN_MASK), "btColar");
+		atalhos.put(KeyStroke.getKeyStroke(KeyEvent.VK_X, KeyEvent.CTRL_DOWN_MASK), "btRecortar");
+		atalhos.put(KeyStroke.getKeyStroke("F8"), "btCompilar");
+		atalhos.put(KeyStroke.getKeyStroke("F9"), "btGerarCodigo");
+		atalhos.put(KeyStroke.getKeyStroke("F1"), "btEquipe");
+		// end
+		
 		btNovo = new JButton("novo [ctrl-n]");
 		btNovo.addActionListener(acNovo);
 		defineBotao(btNovo);
 		paBotoes.add(btNovo);
-
+		
 		btAbrir = new JButton("abrir [ctrl-a]");
-		btAbrir.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try {
-					abrir();
-				} catch (IOException e1) {
-					//TODO
-					e1.printStackTrace();
-				}
-			}
-			
-		});
+		btAbrir.addActionListener(acAbrir);
 		defineBotao(btAbrir);
 		paBotoes.add(btAbrir);
-
+		
 		btSalvar = new JButton("salvar [ctrl-s]");
-		btSalvar.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try {
-					salvar();
-				} catch (IOException e1) {
-					// TODO
-					e1.printStackTrace();
-				}
-			}
-		});
+		btSalvar.addActionListener(acSalvar);
 		defineBotao(btSalvar);
 		paBotoes.add(btSalvar);
-
+		
 		btCopiar = new JButton("copiar [ctrl-c]");
-		btCopiar.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				copiar();
-			}
-		});
+		btCopiar.addActionListener(acCopiar);
 		defineBotao(btCopiar);
 		paBotoes.add(btCopiar);
-
+		
 		btColar = new JButton("colar [ctrl-v]");
-		btColar.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				colar();
-			}
-		});
+		btColar.addActionListener(acColar);
 		defineBotao(btColar);
 		paBotoes.add(btColar);
-
+		
 		btRecortar = new JButton("recortar [ctrl-x]");
-		btRecortar.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				recortar();
-			}
-		});
+		btRecortar.addActionListener(acRecortar);
 		defineBotao(btRecortar);
 		paBotoes.add(btRecortar);
-
+		
 		btCompilar = new JButton("compilar [F8]");
-		btCompilar.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				compilar();
-			}
-		});
+		btCompilar.addActionListener(acCompilar);
 		defineBotao(btCompilar);
 		paBotoes.add(btCompilar);
-
+		
 		btGerarCodigo = new JButton("gerar c\u00F3digo [F9]");
-		btGerarCodigo.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				gerarCodigo();
-			}
-		});
+		btGerarCodigo.addActionListener(acGerarCodigo);
 		defineBotao(btGerarCodigo);
 		paBotoes.add(btGerarCodigo);
-
+		
 		btEquipe = new JButton("equipe [F1]");
-		btEquipe.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				equipe();
-			}
-		});
+		btEquipe.addActionListener(acEquipe);
 		defineBotao(btEquipe);
 		paBotoes.add(btEquipe);
-
+		
 		paEditor = new JPanel();
 		getContentPane().add(paEditor, BorderLayout.CENTER);
 		paEditor.setLayout(new BorderLayout(0, 0));
-
+		
 		spEditor = new JScrollPane();
 		spEditor.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 		spEditor.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		paEditor.add(spEditor, BorderLayout.CENTER);
-
+		
 		taEditor = new JTextArea();
 		taEditor.setBorder(new NumberedBorder());
-		taEditor.getInputMap(JPanel.WHEN_FOCUSED).put(
-				KeyStroke.getKeyStroke(KeyEvent.VK_A, KeyEvent.CTRL_DOWN_MASK),
-				"none");
+		taEditor.getInputMap(JPanel.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_A, KeyEvent.CTRL_DOWN_MASK), "none");
 		spEditor.setViewportView(taEditor);
-
+		
 		spMensagens = new JScrollPane();
-		spMensagens
-				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		spMensagens
-				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		spMensagens.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		spMensagens.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 		spMensagens.setPreferredSize(new Dimension(0, 100));
 		paEditor.add(spMensagens, BorderLayout.SOUTH);
-
+		
 		taMensagens = new JTextArea();
 		taMensagens.setEditable(false);
-		taMensagens.getInputMap(JPanel.WHEN_FOCUSED).put(
-				KeyStroke.getKeyStroke(KeyEvent.VK_A, KeyEvent.CTRL_DOWN_MASK),
-				"none");
+		taMensagens.getInputMap(JPanel.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_A, KeyEvent.CTRL_DOWN_MASK), "none");
 		spMensagens.setViewportView(taMensagens);
-
+		
 		paStatusBar = new JPanel();
 		paStatusBar.setPreferredSize(new Dimension(50, 25));
 		paStatusBar.setAlignmentX(Component.LEFT_ALIGNMENT);
 		paStatusBar.setBorder(new LineBorder(new Color(0, 0, 0)));
 		getContentPane().add(paStatusBar, BorderLayout.SOUTH);
 		paStatusBar.setLayout(new BorderLayout(0, 0));
-
+		
 		lblStatusbar = new JLabel("StatusBar");
 		lblStatusbar.setHorizontalAlignment(SwingConstants.LEFT);
 		paStatusBar.add(lblStatusbar);
-
+		
 		paEsqStatusBar = new JPanel();
 		paEsqStatusBar.setPreferredSize(new Dimension(5, 10));
 		paStatusBar.add(paEsqStatusBar, BorderLayout.WEST);
-		addKeyListener(new KeyListener() {
-			
-			@Override
-			public void keyTyped(KeyEvent e) {
-				
-			}
-			
-			@Override
-			public void keyReleased(KeyEvent e) {
-				
-			}
-			
-			@Override
-			public void keyPressed(KeyEvent e) {
-				switch (e.getKeyCode()){
-					case KeyEvent.VK_A:
-						if (e.isControlDown()){
-							try {
-								abrir();
-							} catch (IOException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-						}
-						break;
-					case KeyEvent.VK_N:
-						if (e.isControlDown()){
-							novo();
-						}
-						break;
-					case KeyEvent.VK_S:
-						if (e.isControlDown()){
-						try {
-							salvar();
-						} catch (IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-					}
-					break;
-					case KeyEvent.VK_C:
-						if (e.isControlDown()){
-							copiar();
-						}
-						break;
-					case KeyEvent.VK_V:
-						if (e.isControlDown()){
-							colar();
-						}
-						break;
-					case KeyEvent.VK_X:
-						if (e.isControlDown()){
-							recortar();
-						}
-						break;
-					case KeyEvent.VK_F8:
-						compilar();
-						break;
-					case KeyEvent.VK_F9:
-						gerarCodigo();
-						break;
-					case KeyEvent.VK_F1:
-						equipe();
-						break;
-					}
-				}
-		});
 	}
-
-	private void defineBotao(JButton botao) {
+	
+	private void defineBotao(final JButton botao) {
 		botao.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		botao.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		botao.setBorder(new MatteBorder(1, 1, 1, 1, new Color(0, 0, 0)));
 		botao.setPreferredSize(new Dimension(95, 30));
 		botao.setMaximumSize(new Dimension(95, 30));
 	}
-
+	
 	private void novo() {
 		taEditor.setText("");
 		nomeArquivo = "";
 	}
-
+	
 	private void abrir() throws IOException {
 		JFileChooser fileChooser = new JFileChooser();
 		if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
 			File file = fileChooser.getSelectedFile();
 			nomeArquivo = file.getAbsolutePath();
-
+			
 			BufferedReader br = new BufferedReader(new FileReader(file));
 			try {
 				String line;
@@ -326,9 +300,9 @@ public class FrmCompilador extends JFrame {
 			}
 		}
 	}
-
+	
 	private void salvar() throws IOException {
-		if (nomeArquivo.isEmpty()){
+		if (nomeArquivo.isEmpty()) {
 			JFileChooser fileChooser = new JFileChooser();
 			if (fileChooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
 				nomeArquivo = fileChooser.getSelectedFile().getAbsolutePath();
@@ -343,30 +317,48 @@ public class FrmCompilador extends JFrame {
 			br.close();
 		}
 	}
-
+	
 	private void copiar() {
 		taEditor.copy();
 	}
-
+	
 	private void colar() {
 		taEditor.paste();
 	}
-
+	
 	private void recortar() {
 		taEditor.cut();
 	}
-
+	
 	private void compilar() {
-		System.out.println("Compilar");
+		adicionaMensagem("compilação de programas ainda não foi implementada");
 	}
-
+	
 	private void gerarCodigo() {
-		System.out.println("Gerar código");
+		adicionaMensagem("geração de código ainda não foi implementada");
 	}
-
+	
+	private void adicionaMensagem(final String novaMensagem) {
+		ArrayList<String> novaListaMensagens = new ArrayList<>();
+		int i = 0;
+		String mensagem;
+		novaListaMensagens.add(novaMensagem);
+		while ((i < 5) && (i < mensagens.size())) {
+			mensagem = mensagens.get(i);
+			novaListaMensagens.add(mensagem);
+			i++;
+		}
+		mensagens = novaListaMensagens;
+		taMensagens.setEditable(true);
+		try {
+			taMensagens.setText(mensagens.toString());
+		} finally {
+			taMensagens.setEditable(false);
+		}
+	}
+	
 	private void equipe() {
-		JOptionPane.showMessageDialog(this, "Equipe: Fredy Schlag \n"
-				+ "        Jonathan Souza \n" + "        Paulo Weber");
+		JOptionPane.showMessageDialog(this, "Equipe: Fredy Schlag \n" + "        Jonathan Souza \n" + "        Paulo Weber");
 	}
-
+	
 }
