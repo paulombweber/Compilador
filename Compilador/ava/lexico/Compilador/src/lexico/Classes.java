e091e3e8b3602f3dd194ba3e24fb3f03eaf96764
Fredy Schlag <fredy.schlag@gmail.com>
2014-04-11 19:40:37 -0300

    Arquivos postados no AVA

---
+++ Compilador/ava/lexico/Compilador/src/lexico/Classes.java
@@ -0,0 +1,19 @@
+package lexico;
+
+public enum Classes {
+	;
+
+	public static String get(int id) {
+		return values[id];
+	}
+	
+	private static String[] values = new String[]{
+	"EPSILON", "DOLLAR", "identificador   ", "constanteInteger", "constanteFloat", "constanteString",
+	"símbolo especial", "símbolo especial", "símbolo especial", "símbolo especial", "símbolo especial",
+	"símbolo especial", "símbolo especial", "símbolo especial", "símbolo especial", "símbolo especial",
+	"símbolo especial", "símbolo especial", "símbolo especial", "símbolo especial", "símbolo especial",
+	"símbolo especial", "símbolo especial", "símbolo especial",
+	"palavra reservada", "palavra reservada", "palavra reservada", "palavra reservada", "palavra reservada",
+	"palavra reservada", "palavra reservada", "palavra reservada", "palavra reservada", "palavra reservada",
+	"palavra reservada", "palavra reservada", "palavra reservada", "palavra reservada", "palavra reservada", "palavra reservada"};
+}
---
+++ Compilador/ava/lexico/Compilador/src/lexico/Token.java
@@ -0,0 +1,47 @@
+package lexico;
+
+public class Token
+{
+    private int id;
+    private String lexeme;
+    private int line;
+    private int column;
+    private int position;
+
+    public Token(int id, String lexeme, int line, int column, int position)
+    {
+        this.id = id;
+        this.lexeme = lexeme;
+        this.line = line;
+        this.column = column;
+        this.position = position;
+    }
+
+    public final int getId()
+    {
+        return id;
+    }
+
+    public final String getLexeme()
+    {
+        return lexeme;
+    }
+
+    public final int getLine()
+    {
+        return line;
+    }
+    
+    public final int getColumn() {
+    	return column;
+    }
+    
+    public final int getPosition() {
+    	return position;
+    }
+
+    public String toString()
+    {
+        return id+" ( "+lexeme+" ) @ "+ getLine() + ":" + getColumn() + ":" + getPosition();
+    };
+}
