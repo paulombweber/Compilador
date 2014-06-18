2b7eb2af3aefde26b2d07643a09bd570c9658a0b
pmbweber <pmbweber@MC-3346.furb.br>
2014-06-18 20:05:50 -0300

    init

---
+++ Compilador/src/comum/Variavel.java
@@ -0,0 +1,26 @@
+package comum;
+
+import semantico.TipoDado;
+
+public class Variavel {
+
+	private TipoDado tipo;
+	private String nome;
+	
+	public Variavel(TipoDado tipo, String nome) {
+		this.tipo = tipo;
+		this.nome = nome;
+	}
+	public TipoDado getTipo() {
+		return tipo;
+	}
+	public void setTipo(TipoDado tipo) {
+		this.tipo = tipo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
